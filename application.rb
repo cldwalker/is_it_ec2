c8472113338e98840f984f3c4f2186440c23013c
Gabriel Horner <gabriel.horner@gmail.com>
2012-04-22 17:17:11 -0400

    works with IP or hostname

--- application.rb
+++ application.rb
@@ -1,10 +1,18 @@
 require 'bundler'
 Bundler.require(:default, (ENV['RACK_ENV'] || :development).to_sym)
 
+$:.unshift File.dirname(__FILE__) + '/lib'
+require 'is_it_ec2'
+
 module My
   class Application < Sinatra::Base
     get '/' do
       haml :index
+    end
+
+    get '/:ip' do
+      @answer = IsItEc2.ask(params[:ip]) ? 'YEP' : 'NOPE'
+      haml :ip
     end
   end
 end
