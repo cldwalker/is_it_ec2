fa6d7245ecfcb7fb476626a78c4f2cceb40391f2
Gabriel Horner <gabriel.horner@gmail.com>
2012-04-27 08:20:43 -0400

    a lil more stylin, switch lookup to form

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,10 @@
 (function($) {
   $(function() {
-    $('input.lookup').click(function() {
+    $('form.lookup').submit(function() {
       var ip = $('input#ip').val();
       (ip === '') ? alert('Lookup is blank. Please enter a value.') :
         window.location = "/" + ip;
+      return false;
     });
   });
 })(jQuery);
